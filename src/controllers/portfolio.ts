82316452bc01f3b3cab47195a4bbd63904eb83c9
Dan Matthews <dan@cryptobuilders.io>
2020-01-15 12:48:53 -0500

    add initialPrice to portfolio

--- src/controllers/portfolio.ts
+++ src/controllers/portfolio.ts
@@ -36,24 +36,24 @@ router.get("/awp++", async (ctx: Koa.Context) => {
 
 router.get("/awp+++", async (ctx: Koa.Context) => {
     const awp = new Portfolio([
-        { ratio: 0.27, asset: new Stock('VTI') }, 
-        { ratio: 0.36, asset: new Stock('TLT') }, 
-        { ratio: 0.135, asset: new Stock('IEI') }, 
-        { ratio: 0.0675, asset: new Stock('GLD') },
-        { ratio: 0.0675, asset: new Stock('GSG') },
-        { ratio: 0.035, asset: new Crypto('BTC') },
-        { ratio: 0.035, asset: new Crypto('ETH') },
+        { initialPrice: 166.1, ratio: 0.27, asset: new Stock('VTI') }, 
+        { initialPrice: 138.155, ratio: 0.36, asset: new Stock('TLT') }, 
+        { initialPrice: 126.205, ratio: 0.135, asset: new Stock('IEI') }, 
+        { initialPrice: 146.87, ratio: 0.0675, asset: new Stock('GLD') },
+        { initialPrice: 16.0456, ratio: 0.0675, asset: new Stock('GSG') },
+        { initialPrice: 8047.017698, ratio: 0.035, asset: new Crypto('BTC') },
+        { initialPrice: 140.8951918, ratio: 0.035, asset: new Crypto('ETH') },
 
         //DeFi Basket
-        { ratio: 0.003333, asset: new Crypto('LINK') }, 
-        { ratio: 0.003333, asset: new Crypto('MKR') }, 
-        { ratio: 0.003333, asset: new Crypto('ZRX') }, 
-        { ratio: 0.003333, asset: new Crypto('SNX') }, 
-        { ratio: 0.003333, asset: new Crypto('REN') }, 
-        { ratio: 0.003333, asset: new Crypto('LRC') }, 
-        { ratio: 0.003333, asset: new Crypto('KNC') }, 
-        { ratio: 0.003333, asset: new Crypto('BNT') }, 
-        { ratio: 0.003333, asset: new Crypto('MLN') }, 
+        { initialPrice: 2.221262472, ratio: 0.003333, asset: new Crypto('LINK') }, 
+        { initialPrice: 471.4785208, ratio: 0.003333, asset: new Crypto('MKR') }, 
+        { initialPrice: 0.2068537448, ratio: 0.003333, asset: new Crypto('ZRX') }, 
+        { initialPrice: 1.053648178, ratio: 0.003333, asset: new Crypto('SNX') }, 
+        { initialPrice: 0.04409045937, ratio: 0.003333, asset: new Crypto('REN') }, 
+        { initialPrice: 0.02262847992, ratio: 0.003333, asset: new Crypto('LRC') }, 
+        { initialPrice: 0.1977862246, ratio: 0.003333, asset: new Crypto('KNC') }, 
+        { initialPrice: 0.2223841873, ratio: 0.003333, asset: new Crypto('BNT') }, 
+        { initialPrice: 3.341016965, ratio: 0.003333, asset: new Crypto('MLN') }, 
     ]);
     ctx.body = await awp.getState();
 });
